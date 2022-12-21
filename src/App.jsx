73b086d9852549348b9e106983c27db8d74f77dd
Sreejith K <sreejith.k@omniflo.in>
2022-12-21 12:57:45 +0530

    Feat : Added React Query Dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import React, { createContext, useState } from "react";
 import { RouterConfig } from "./config/RouterConfig";
+import { QueryClient, QueryClientProvider } from "react-query";
 export const distanceContext = createContext();
+
+const queryClient = new QueryClient()
+
 
 function App() {
   const [storeDetails, setStoreDetails] = useState({
@@ -11,9 +15,11 @@ function App() {
   });
 
   return (
+    <QueryClientProvider client={queryClient}>
     <distanceContext.Provider value={{ storeDetails, setStoreDetails }}>
       <RouterConfig className="m-0 p-0" />
     </distanceContext.Provider>
+    </QueryClientProvider>
   );
 }
 export default App;
