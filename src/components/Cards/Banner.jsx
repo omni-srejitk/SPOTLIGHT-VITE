0e976922239fa86be5443fb77a8ed7ca865ed7ed
Sreejith K <sreejith.k@omniflo.in>
2022-12-27 18:31:41 +0530

    Feat : Added Banner Component

---
+++ src/components/Cards/Banner.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ShiningStarsAnimation } from "../ShiningStarsAnimation";
+
+export const Banner = ({ children, color }) => {
+  return (
+    <div
+      className={`relative flex h-[60vh] min-h-[30rem]  w-full flex-col items-center rounded-lg p-2 pt-12 sm:h-[24rem] ${color}`}
+    >
+      <img src="/new left dots.svg" className="absolute left-0 top-4 h-[90%]" />
+      <img
+        src="/new right dots.svg"
+        className="absolute right-0 top-4 h-[90%]"
+      />
+      <div className="absolute right-12 top-4 w-12">
+        <ShiningStarsAnimation />
+      </div>
+      <div className="absolute left-[-0.6rem] top-[-0.7rem] w-[3.5rem]">
+        <img className="" src="/new star.svg" />
+      </div>
+      {children}
+    </div>
+  );
+};
