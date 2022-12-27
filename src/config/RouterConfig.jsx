95e12bbde89041c33b7455b3e303d52f73639f8c
Sreejith K <sreejith.k@omniflo.in>
2022-12-27 19:05:07 +0530

    Feat : Added Additional Routes

--- src/config/RouterConfig.jsx
+++ src/config/RouterConfig.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../Home/Home";
+import LocationDenyPage from "../components/LocationDenyPage";
+import { Error } from "../routes/404/Error";
+import Brand from "../routes/Brand/Brand";
+import Home from "../routes/Home/Home";
+import { Stores } from "../routes/Stores/Stores";
 
 export const RouterConfig = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/:brand" element={<Brand />} />
+      <Route path="/:brand/Stores" element={<Stores />} />
+      <Route path="/:brand/Location_Denied" element={<LocationDenyPage />} />
+      <Route path="/:brand/*" element={<Error />} />
     </Routes>
   );
 };
