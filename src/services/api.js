03ea7af0962e5c2fc02a0e539b134410eea33d29
Sreejith K <sreejith.k@omniflo.in>
2022-12-28 17:47:37 +0530

    Feat : Added Brand Fetching API

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,27 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const brandDetailURL = import.meta.env.VITE_OMNIFLO_API;
 
 
+export const fetchBrandDetails = (brandName) => {
 
- 
+
+    return useQuery(
+      ["brandDetails"],
+
+      () => {
+        return axios.get(brandDetailURL + brandName);
+      },
+
+      {
+        select: (data) => {
+           
+          const res = data?.data;
+          const { name, story, testimonial, rating, logo } = res;
+
+          return { name, story, testimonial, rating, logo };
+        },
+      }
+    );
+  };
