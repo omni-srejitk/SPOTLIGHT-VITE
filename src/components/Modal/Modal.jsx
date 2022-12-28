fa3561ff79e5a42377ef979acd3134318e333bb2
Sreejith K <sreejith.k@omniflo.in>
2022-12-28 16:38:39 +0530

    Refactor : Updated Modal Code

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,19 +1,26 @@
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import ReactDOM from "react-dom";
 
-export const Modal = ({ isModalOpen, onClose, children }) => {
+export const Modal = ({ isModalOpen, onClose, children, position }) => {
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 z-50" onClick={onClose}>
-      <div className="fixed inset-0 bg-white/30"></div>
-      {isModalOpen ? (
-        <div
-          className=" fixed top-1/2 left-1/2 h-fit w-fit -translate-x-1/2 -translate-y-1/2"
-          onClick={(e) => e.stopPropagation()}
-        >
-          {children}
-        </div>
-      ) : null}
-    </div>,
+    <AnimatePresence mode="wait">
+      <div className="fixed inset-0 z-50" onClick={onClose}>
+        <div className="fixed inset-0 bg-white/30"></div>
+        {isModalOpen && (
+          <motion.div
+            key="Modal"
+            initial={{ y: 100 }}
+            animate={{ y: 0 }}
+            exit={{ y: -100 }}
+            className={`${position}  fixed h-fit w-fit`}
+            onClick={(e) => e.stopPropagation()}
+          >
+            {children}
+          </motion.div>
+        )}
+      </div>
+    </AnimatePresence>,
     document.getElementById("portal")
   );
 };
