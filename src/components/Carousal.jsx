e645b8d37a61cdab34f8c2b7a3484f81350d1d85
Sreejith K <sreejith.k@omniflo.in>
2022-12-28 16:38:59 +0530

    Fix : Corrected Invalid Data

--- src/components/Carousal.jsx
+++ src/components/Carousal.jsx
@@ -1,14 +1,12 @@
-import React, { useRef } from "react";
-
-import { useStore } from "../context/storeContext";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { fetchBrandDetails } from "../services/api";
 import { Card } from "./Cards/Card";
 
 export const Carousal = () => {
-  let {
-    storeDetails: { information },
-  } = useStore();
-
-  let stars = 4;
+  const { brand } = useParams();
+  const { data, isFetching } = fetchBrandDetails(brand);
+  let stars = data?.rating?.slice(0, 3);
 
   const ratingStar = Array.from({ length: 5 }, (elem, index) => {
     let number = index + 0.5;
@@ -32,7 +30,7 @@ export const Carousal = () => {
   });
 
   return (
-    <div className=" no-scrollbar my-4 flex h-72 w-full  justify-start gap-4 overflow-x-auto bg-black  py-2 text-base text-black">
+    <div className="my-4 flex h-72 w-full justify-start gap-4  overflow-x-auto bg-black px-0 py-2  text-base text-black scrollbar-none">
       <Card>
         <div className="relative h-full rounded-lg border-2 border-yellow-500/20 bg-yellow-200 p-2 text-center text-black">
           <div className="absolute bottom-0 left-0 z-0 w-40">
@@ -41,30 +39,30 @@ export const Carousal = () => {
 
           <div className="ml-6 mt-5 mb-2 flex h-1/4 w-4/5 flex-row items-center justify-start">
             <div className="z-10 flex h-16 w-16 items-center justify-center rounded-[2.5rem] border-[1px] border-black bg-white p-3">
-              <img className=" object-contain " src={information?.logo} />
+              <img className=" object-contain " src={data?.logo} />
             </div>
             <p
-              className="z-10 ml-3 cursor-pointer text-xl font-semibold text-[black]"
-              onClick={() => window.open(`${information?.url}`)}
+              className="z-10 ml-3 cursor-pointer text-xl font-semibold capitalize text-[black]"
+              onClick={() => window.open(`${data?.url}`)}
             >
-              {information.name}
+              {data.name}
             </p>
           </div>
           <p
-            className="relative z-10 max-h-[7rem] w-full overflow-y-auto px-4 text-start text-lg leading-6 line-clamp-4"
-            title={information?.story}
+            className="line-clamp-4 relative z-10 max-h-[7rem] w-full overflow-y-auto px-4 text-start text-lg leading-6"
+            title={data?.story}
           >
-            {information?.story}
+            {data?.story}
           </p>
         </div>
       </Card>
 
       <Card>
-        <div className="relative h-full rounded-lg border-2 border-yellow-500/20 bg-black p-2 text-center text-yellow-400">
-          <p className="relative z-10 mt-3 p-4 text-xl font-semibold">
+        <div className="relative h-full rounded-lg border-2 border-yellow-500/20 bg-yellow-200 p-2 text-center text-yellow-400">
+          <p className="relative z-10 mt-3 p-4 text-xl font-semibold text-black">
             Happy customer
           </p>
-          <span className="relative z-10">{information?.testimonial}</span>
+          <span className="relative z-10 text-black">{data?.testimonial}</span>
         </div>
       </Card>
 
@@ -75,7 +73,7 @@ export const Carousal = () => {
           </div>
           <div className="mt-4 flex">{ratingStar}</div>
           <p className="relative z-10 text-gray-700">
-            {information?.rating?.slice(5, -2)}
+            {data?.rating?.slice(5, -2)}
           </p>
         </div>
       </Card>
