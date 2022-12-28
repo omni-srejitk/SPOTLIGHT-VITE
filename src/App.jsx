3a00f664f777ab77f26e95141ea38392640c8af7
Sreejith K <sreejith.k@omniflo.in>
2022-12-28 14:49:49 +0530

    Refactor : Moved Providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import "./App.css";
 import React from "react";
 import { RouterConfig } from "./config/RouterConfig";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter as Router } from "react-router-dom";
-import { StoreProvider } from "./context/storeContext";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 function App() {
-  const queryClient = new QueryClient();
   return (
-    <Router>
-      <QueryClientProvider client={queryClient}>
-        <StoreProvider>
-          <Header />
-          <RouterConfig />
-          <Footer />
-        </StoreProvider>
-      </QueryClientProvider>
-    </Router>
+    <div className="App bg-black">
+      <RouterConfig />
+    </div>
   );
 }
 export default App;
