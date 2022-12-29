9e8b32126b6cde287877d51c9855e189df0b1642
Sreejith K <sreejith.k@omniflo.in>
2022-12-29 11:25:37 +0530

    Fix : DOM addition cleanup

--- src/components/LoadComponent.jsx
+++ src/components/LoadComponent.jsx
@@ -11,11 +11,16 @@ export const LoadComponent = () => {
       autoplay: true,
       animationData: LoadComp,
     });
+
+    return () => {
+      lottie.destroy();
+    };
   }, []);
+
   return (
-    <div className=" h-[100vh]">
+    <div className=" h-screen">
       <div className="h-[60vh] pt-24" id="container"></div>
-      <p className="mx-auto mt-4 w-full text-center text-[1.5rem] sm:left-[20rem]">
+      <p className="mx-auto mt-4 w-full text-center text-2xl sm:left-[20rem]">
         Searching store near you...
       </p>
     </div>
