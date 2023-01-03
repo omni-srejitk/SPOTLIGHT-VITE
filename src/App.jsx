87776f9d6f6bd3d68add27122f74dc7163e6ccf3
Sreejith K <sreejith.k@omniflo.in>
2023-01-03 16:40:17 +0530

    Feat : Added GTM

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import "./App.css";
 import React from "react";
 import { RouterConfig } from "./config/RouterConfig";
-
+import TagManager from "react-gtm-module";
 function App() {
+  if (import.meta.env.PROD == true) {
+    const tagManagerArgs = {
+      gtmId: import.meta.env.VITE_GTM_TOKEN,
+    };
+
+    TagManager.initialize(tagManagerArgs);
+  }
+
   return (
     <div className="App bg-black">
       <RouterConfig />
